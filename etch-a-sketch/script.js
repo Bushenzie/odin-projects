326d7cca762e619a95310cc5128f3befd72af65a
Bushenzie <buchenzii@gmail.com>
2022-03-07 21:59:36 +0100

    Add field-picker

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,21 +1,65 @@
-MakeField(128, 128);
+let containerSize = 728;
 
-function MakeField(x, y) {
-    const container = document.createElement("div");
-    const containerSize = 512;
+const container = document.createElement("div");
+const select = document.getElementById("field-select");
+
+MakeBorder()
+
+select.addEventListener("change", (e) => {
+    switch (select.options[select.selectedIndex].value) {
+        case "16x16":
+            DeleteField()
+            MakeField(16, 16);
+            break;
+        case "24x24":
+            DeleteField()
+            MakeField(24, 24);
+            break;
+        case "32x32":
+            DeleteField()
+            MakeField(32, 32);
+            break;
+        case "48x48":
+            DeleteField()
+            MakeField(48, 48);
+            break;
+        case "64x64":
+            DeleteField()
+            MakeField(64, 64);
+            break;
+    }
+
+});
+
+
+function MakeBorder() {
     container.classList.add("container");
     container.style.width = containerSize + "px";
     container.style.height = containerSize + "px";
     document.body.appendChild(container);
+}
+
+
+function MakeField(x, y) {
+
 
     for (let i = 0; i < x * y; i++) {
         let newEl = document.createElement("div");
-        newEl.classList.add("item");
+        newEl.classList.add("block");
         newEl.style.width = containerSize / x + "px";
         newEl.style.height = containerSize / y + "px";
-        newEl.addEventListener("dragenter", () => {
+        newEl.addEventListener("dragover", () => {
+            newEl.classList.add("clicked");
+        })
+        newEl.addEventListener("click", () => {
             newEl.classList.add("clicked");
         })
         container.appendChild(newEl);
     }
+}
+
+function DeleteField() {
+    document.querySelectorAll(".block").forEach(block => {
+        container.removeChild(block);
+    })
 }
