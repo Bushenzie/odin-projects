43103dccfadc13b604cab587a550e116a3a11a50
Bushenzie <buchenzii@gmail.com>
2022-03-11 19:01:59 +0100

    Prototype v2

--- calculator/script.js
+++ calculator/script.js
@@ -1,15 +1,17 @@
 
-//Elements
+//TODO dot fix
+//TODO design
+//TODO adding při resultu hází předchozí číslo
 
-const prevDisplay = document.querySelector(".input-upper");
+
+
+
+//Elements
 const display = document.querySelector(".input");
 
 //Variables
-let numberOne = 0, numberTwo = 0;
-let operator;
-let operatorSelected = false;
-let result = 0;
-
+let nums = [], iteration = 0, operator, result;
+let operatorSelected = false, oneDotPerNum = false;
 
 //EventListeners
 
@@ -22,45 +24,31 @@ document.querySelectorAll(".item").forEach(item => {
         item.addEventListener("click", AbsoluteClear);
     } else if (item.value === "=") {
         item.addEventListener("click", () => {
-            if (!operator) {
-                Calculate("none");
+            if (nums.length === 1 && operator && display.textContent !== "") {
+                nums.push(Number(display.textContent));
+                Calculate(operator);
             } else {
                 Calculate(operator);
             }
         });
     } else {
-        item.addEventListener("click", function () {
-            if (!operatorSelected) {
-                this.classList.add("selected");
-
-                operator = item.value;
-
-                operatorSelected = true;
-
-                numberOne = Number(display.textContent);
-                console.log(numberOne);
-
-                DisplayClear();
-
-            } else if (operatorSelected) {
-
-                numberTwo = Number(display.textContent);
-                console.log(NumberTwo);
-
-                operatorSelected = false;
-
-                Calculate(operator);
-
-                numberOne = result;
-
-                numberTwo = 0;
-
-                ResetOperations();
+        item.addEventListener("click", () => {
+            operator = item.value;
+            if (iteration === 0) {
+                nums.push(Number(display.textContent));
+                iteration++;
             }
+
+            Debug();
+            DisplayClear();
         });
     }
 })
 
+
+function Debug() {
+    console.log(nums);
+}
 
 function DisplayUpdate(value) {
     display.textContent += value;
@@ -71,42 +59,75 @@ function DisplayClear() {
 }
 
 function AbsoluteClear() {
-    result = 0;
-    numberOne = 0;
-    numberTwo = 0;
-    operator = "";
+    result = undefined;
+    nums.splice(0, nums.length);
+    iteration = 0;
+    operator = undefined;
     operatorSelected = false;
     display.textContent = "";
     ResetOperations();
+    Debug();
 }
 
 function Calculate(operator) {
+    Debug();
     switch (operator) {
         case "+":
-            result = add(numberOne, numberTwo);
+            result = add(nums[0], nums[1]);
+            ArrayClear()
+            nums.push(result);
+
+
+            ResetOperations();
+            DisplayClear();
             DisplayUpdate(result);
             break;
         case "-":
-            result = subtr(numberOne, numberTwo);
+            result = subtr(nums[0], nums[1]);
+            ArrayClear()
+            nums.push(result);
+
+
+            ResetOperations();
+            DisplayClear();
+            DisplayUpdate(result);
             break;
         case "*":
-            result = multiply(numberOne, numberTwo);
+            result = multiply(nums[0], nums[1]);
+            ArrayClear()
+            nums.push(result);
+
+
+            ResetOperations();
+            DisplayClear();
             DisplayUpdate(result);
             break;
         case "/":
-            result = divide(numberOne, numberTwo)
+            result = divide(nums[0], nums[1]);
+            ArrayClear()
+            nums.push(result);
+
+
+            ResetOperations();
+            DisplayClear();
             DisplayUpdate(result);
             break;
-        case "none":
-            DisplayUpdate(display.value);
+        default:
+            display.textContent = display.textContent;
             break;
     }
+}
+
+function ArrayClear() {
+    nums.splice(0, nums.length);
 }
 
 function ResetOperations() {
     document.querySelectorAll(".oper").forEach(operator => {
         operator.classList.remove("selected");
     })
+    operator = undefined;
+    oneDotPerNum = false;
 }
 
 const add = (num1, num2) => num1 + num2;
