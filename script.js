5a8c78dbab89effd17e8df6ff79eb8fdec3f67b3
Bushenzie <buchenzii@gmail.com>
2022-03-18 19:28:56 +0100

    Link fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 document.querySelectorAll(".item").forEach(item => {
     item.addEventListener("click", () => {
-        window.open(`/${item.id}`, '_blank');
+        window.open(`https://bushenzie.github.io/odin-projects/${item.id}/index.html`, '_blank');
     })
 })
