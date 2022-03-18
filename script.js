cb1c2b03519797a5cb0a033b4617ac63a68d88ec
Bushenzie <buchenzii@gmail.com>
2022-03-18 19:31:25 +0100

    Link repair

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 document.querySelectorAll(".item").forEach(item => {
     item.addEventListener("click", () => {
-        window.open(`https://bushenzie.github.io/odin-projects/${item.id}/index.html`, '_blank');
+        window.open(`https://bushenzie.github.io/odin-projects/${item.id}`, '_blank');
     })
 })
